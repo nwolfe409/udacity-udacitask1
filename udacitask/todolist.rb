bd9a72037d63186489e4c79dbb91fdeeb66fb6d6
Nathan Wolfe <nwolfe409@gmail.com>
2016-02-26 04:03:27 +0000

    Working base code to meet requiremets of project

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -1,25 +1,49 @@
 # todolist.rb
 
 class TodoList
-     attr_reader :title, :items
+  attr_reader :title, :items
 
-     def initialize(list_title) # Initialize todo list with a title and no items
-        @title = list_title
-        @items = Array.new # Starts empty! No Items yet!
-     end
+  def initialize(list_title) # Initialize todo list with a title and no items
+    @title = list_title
+    @items = Array.new # Starts empty! No Items yet!
+  end
      
-      def add_item(new_item)    # Creates a new Item and adds it to the array of Items
-        item = Item.new(new_item)
-        @items.push(item)
+  def add_item(new_item)    # Creates a new Item and adds it to the array of Items
+    item = Item.new(new_item)
+    @items.push(item)
+  end
+    
+  def remove_item(num)
+    @items.delete_at(num-1)
+  end
+    
+  def complete(num)
+    @items[num-1].complete_item
+  end
+  
+  def print_list
+    puts "*" * 25
+    puts "#{title}"
+    puts "*" * 25
+    @items.each_with_index do |item, index|
+      puts "#{index + 1} - #{item.description}  Completed: #{item.completed_status}"
     end
-     
+  end 
+  
+  def update_title(new_title)
+    @title = new_title
+  end
      
 end
 
 class Item
   attr_reader :description, :completed_status
   def initialize(item_description)  # Initialize item with a description and marked as not complete
-        @description = item_description
-        @completed_status = false
-     end 
+    @description = item_description
+    @completed_status = false
+  end
+     
+  def complete_item
+    @completed_status=true
+  end
 end
--- udacitask/udacitask.rb
+++ udacitask/udacitask.rb
@@ -1,23 +1,37 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+my_list = TodoList.new("List of The Things to do")
 
 # Add four new items
+my_list.add_item "Bathe the cat"
+my_list.add_item "Walk the dog"
+my_list.add_item "Finish solving world peace"
+my_list.add_item "Go shopping"
 
 # Print the list
+my_list.print_list
 
 # Delete the first item
+my_list.remove_item 1
 
 # Print the list
+my_list.print_list
 
 # Delete the second item
+my_list.remove_item 2
 
 # Print the list
+my_list.print_list
 
 # Update the completion status of the first item to complete
+my_list.complete(1)
 
 # Print the list
+my_list.print_list
 
 # Update the title of the list
+my_list.update_title "Stuff"
 
 # Print the list
+my_list.print_list
