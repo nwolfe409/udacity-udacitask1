1b50a614f8fec8db02d79cf0106641675f0ce3d5
Nathan Wolfe <nwolfe409@gmail.com>
2016-02-27 02:36:48 +0000

    refactored code ready for submission

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -13,42 +13,39 @@ class TodoList
     @items.push(item)
   end
     
-  def remove_item(num)
+  def remove_item(num) #removes item from array based on the list number shown in print_list
     @items.delete_at(num-1)
   end
     
-  def complete(num)
+  def complete(num) #marks item as complete
     @items[num-1].complete_item
   end
   
-  def print_list
+  def print_list #prints the current contents of the todo list
     puts "*" * 25
     puts "#{title}"
     puts "*" * 25
-    @items.each_with_index do |item, index|
-      puts "#{index + 1} - #{item.description}  Completed: #{item.completed_status} Due date: #{item.due_date}"
-    end
+    @items.each_with_index { |item, index| puts "#{index + 1} - #{item.description}  Completed: #{item.completed_status} Due date: #{item.due_date}" }
   end 
   
-    def print_incomplete
+  def print_incomplete # prints only the imcomplete items
+    puts "*" * 25
+    puts "#{title}"
+    puts "*" * 25
+    puts "Incomplete items"
+    puts "*" * 25
     @items.each_with_index do |item, index|
-      if item.completed_status == false 
-          puts "#{index + 1} - #{item.description}  Completed: #{item.completed_status}"
-    end
-      end
-  end
-  
-  def remove_completed
-    @items.each_with_index do |item, index|
-      puts "iteration #{index}"
-      if item.completed_status == true 
-        @items.delete_at(index)
-      end
+      puts "#{index + 1} - #{item.description} Completed: #{item.completed_status}" if item.completed_status == false 
     end
   end
-      
   
-  def update_title(new_title)
+  def remove_completed #removes the completed items from the list
+    @items.each_with_index do |item, index|
+      @items.delete_at(index) if item.completed_status == true
+    end
+  end
+
+  def update_title(new_title) #updates the title of the list
     @title = new_title
   end
      
